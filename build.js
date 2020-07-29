eaab7dfdb9db3c7b278e6c34a69fd0a30d46edd0
YXL <chenxin.lan.76@gmail.com>
2020-07-29 11:36:32 +0800

    🐞fix(wakatime):

--- build.js
+++ build.js
@@ -1,6 +1,6 @@
 const https = require("https");
 const xml2js = require("xml2js");
-const { writeFileSync } = require("fs");
+const { readFileSync, writeFileSync } = require("fs");
 
 const mouth = {
   Jan: 1,
@@ -17,46 +17,39 @@ const mouth = {
   Dec: 12,
 };
 
-let weekly = "";
+const weekly = readFileSync(
+  "./fa9ef0deb6800d867c1598195d51bc22/📊 Weekly development breakdown"
+);
+
+let douban = "";
 
 https.get(
-  "https://gist.githubusercontent.com/YXL76/fa9ef0deb6800d867c1598195d51bc22/raw/1d111221bba7161d2c1ed7d93bf6ecd372609932/%25F0%259F%2593%258A%2520Weekly%2520development%2520breakdown",
+  "https://www.douban.com/feed/people/151739065/interests",
+  {
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36",
+    },
+  },
   (res) => {
     res.on("data", (chunk) => {
-      weekly += chunk;
+      douban += chunk;
     });
     res.on("end", () => {
-      let douban = "";
+      xml2js.parseStringPromise(douban).then((result) => {
+        douban = result.rss.channel[0].item.map(({ title, link, pubDate }) => {
+          date = pubDate[0].split(" ");
+          if (title[0].slice(0, 2) === "最近") {
+            title[0] = title[0].slice(2);
+          }
+          return `- ${title[0].slice(0, 2)}[《${title[0].slice(2)}》](${
+            link[0]
+          }) - \`${date[3]}-${mouth[date[2]]}-${date[1]} ${date[4]}\``;
+        });
 
-      https.get(
-        "https://www.douban.com/feed/people/151739065/interests",
-        {
-          headers: {
-            "User-Agent":
-              "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36",
-          },
-        },
-        (res) => {
-          res.on("data", (chunk) => {
-            douban += chunk;
-          });
-          res.on("end", () => {
-            xml2js.parseStringPromise(douban).then((result) => {
-              douban = result.rss.channel[0].item.map(
-                ({ title, link, pubDate }) => {
-                  date = pubDate[0].split(" ");
-                  if (title[0].slice(0, 2) === "最近") {
-                    title[0] = title[0].slice(2);
-                  }
-                  return `- ${title[0].slice(0, 2)}[《${title[0].slice(2)}》](${
-                    link[0]
-                  }) - \`${date[3]}-${mouth[date[2]]}-${date[1]} ${date[4]}\``;
-                }
-              );
-
-              writeFileSync(
-                "./README.md",
-                `## Hi there 👋
+        writeFileSync(
+          "./README.md",
+          `## Hi there 👋
 
 [![GitHub followers](https://img.shields.io/github/followers/YXL76?style=for-the-badge&color=blue)](https://github.com/YXL76?tab=followers)
 [![Website](https://img.shields.io/website?style=for-the-badge&up_message=Blog&url=https%3A%2F%2Fyxl76.net%2F&color=brightgreen)](https://yxl76.net)
@@ -87,11 +80,8 @@ Here are some ideas to get you started:
 - ⚡ Fun fact: ...
 -->
 `
-              );
-            });
-          });
-        }
-      );
+        );
+      });
     });
   }
 );
