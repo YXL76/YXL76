3c3760a2327b96a441e1af745ad93a99a6882602
YXL <chenxin.lan.76@gmail.com>
2020-12-11 21:18:44 +0800

    🎈perf: reduce line height

--- build.js
+++ build.js
@@ -53,7 +53,7 @@ https.get(
 
 ### 🔭 Github stats
 
-![YXL76's github stats](https://github-readme-stats.yxl76.vercel.app/api?username=YXL76&count_private=true&show_icons=true&theme=tokyonight&line_height=33)
+![YXL76's github stats](https://github-readme-stats.yxl76.vercel.app/api?username=YXL76&count_private=true&show_icons=true&theme=tokyonight&line_height=28)
 
 </td>
 
